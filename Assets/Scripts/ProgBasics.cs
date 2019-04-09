a52f6ea375ac4f7aafa064986cd2ced86fccf8de
tobiasscheichl <scheichl.tobias20@gmail.com>
2019-04-09 10:18:51 +0200

    update readme

--- Assets/Scripts/ProgBasics.cs
+++ Assets/Scripts/ProgBasics.cs
@@ -9,22 +9,22 @@ public class ProgBasics : MonoBehaviour {
 
     Debug.Log("Multiplikative Operatoren: * " + (3 * 4));
     Debug.Log("Multiplikative Operatoren:  / " + (6 / 2));
-    Debug.Log("Multiplikative Operatoren:  / " + (4 % 3));
+    Debug.Log("Multiplikative Operatoren:  % " + (4 % 3));
 
-    Debug.Log("Additive Operatoren:  / " + (8 + 2));
-    Debug.Log("Additive Operatoren:  / " + (7 - 6));
+    Debug.Log("Additive Operatoren:  + " + (8 + 2));
+    Debug.Log("Additive Operatoren:  - " + (7 - 6));
 
-    Debug.Log("Relationale Operatoren:  / " + (2 < 5));
-    Debug.Log("Relationale Operatoren:  / " + (5 <= 9)); 
-    Debug.Log("Relationale Operatoren:  / " + (6 > 3));
-    Debug.Log("Relationale Operatoren:  / " + (4 >= 1));
+    Debug.Log("Relationale Operatoren:  < " + (2 < 5));
+    Debug.Log("Relationale Operatoren:  <= " + (5 <= 9)); 
+    Debug.Log("Relationale Operatoren:  > " + (6 > 3));
+    Debug.Log("Relationale Operatoren:  >= " + (4 >= 1));
 
-    Debug.Log("Gleichheits Operatoren:  / " + (4 == 4));
-    Debug.Log("Gleichheits Operatoren:  / " + (3 != 1));
+    Debug.Log("Gleichheits Operatoren:  == " + (4 == 4));
+    Debug.Log("Gleichheits Operatoren:  != " + (3 != 1));
 
-    Debug.Log("Logische Operatoren:  / " + (2 & 7));
-    Debug.Log("Logische Operatoren:  / " + (4 ⎪ 7));
-    Debug.Log("Logische Operatoren:  / " + (6 etc 2));
+    Debug.Log("Logische Operatoren:  & " + (2 & 7));
+    Debug.Log("Logische Operatoren:  ⎪ " + (4 ⎪ 7));
+    Debug.Log("Logische Operatoren:  etc " + ( etc 2));
 
         //erste tests mit operatoren
     }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ProgBasics-TSCH
+
+<div>
+<img src="./Screenshots/Welcome.png" width="250">
+</div>
